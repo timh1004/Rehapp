d1f7eb1439c0792b82b4f7e67e8cddab5613b719
Tim Haug <timhaug@me.com>
2015-06-29 16:38:09 +0200

    Initial Commit

---
+++ Rehapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rehapp
+//
+//  Created by Tim Haug on 29.06.15.
+//  Copyright (c) 2015 Tim Haug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
